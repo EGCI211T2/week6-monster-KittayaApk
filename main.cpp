05dbdd03127a4d04a25cd7e786f36ae54b838ba0
KittayaApk <kittaya.apk@student.mahidol.edu>
2026-02-11 04:34:24 +0000

    continue on Friday

--- main.cpp
+++ main.cpp
@@ -3,9 +3,29 @@ using namespace std;
 
 #include "monster.h"
 
-int main(int argc, char* argv[]) {
 
-    monster A;
+int main() {
+
+    monster c;
+    monster A("Aom", 10, 1);
+    monster B("mali");
+    monster *p;//no constructor called
+    p=&c;
+    p->display();
+    p=&A;
+    p->display();
+    p=new monster("Momo",20);
+    p->display();
+
+
+    //monster x[5]={{"James "},{"Meaw",20},{"Lala"},{"Zane"},{"Am"}};
+   
+  delete p;
+
+ p=new monster[3];
+ delete []p;
+
+
   /*
  
  monster *p,x;
--- monster.h
+++ monster.h
@@ -2,12 +2,32 @@
 #define monster_h
 
 class monster{
+
 private:
 	string name;
 	int hp,potion;
 public:
 	void Attack(monster &);
-          void heal();
-	monster(string, int,int);
+    void heal();
+	monster(string="Anonymous", int=1,int=1);
+ ~monster();
+ void display();
 };
+monster::~monster(){
+    cout<<name<< " is gone."<<endl;
+}
+
+monster::monster(string n, int h, int p) {
+    name = n;
+    hp = h;
+    potion = p;
+    cout << "Monster " << name << " is here" << endl;
+}
+void monster::display() {
+    cout << "Name: " << name 
+         << " HP: " << hp 
+         << " Potion: " << potion << endl;
+}
+
+
 #endif
